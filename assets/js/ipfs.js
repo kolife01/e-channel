89c452cb43e1ee165a31d1dbd70a2f626c54a63b
ookimaki <ookimaki@givinglog.com>
2018-12-18 19:29:22 +0900

    add

--- assets/js/ipfs.js
+++ assets/js/ipfs.js
@@ -18,9 +18,46 @@ IpfsManager.add = function(title,body){
         }).catch(err =>
             console.log(err)
         )
-
     })
+}
 
+IpfsManager.convertQuestion = async function(rows){
+    var outputs = []
+    for (let i = 0; i < rows.length; i++) {
+        await axios.get('https://cloudflare-ipfs.com/ipfs/' + rows[i].body)
+            .then(response => {
+                outputs.push({title:response.data.title, body:response.data.body})
+            }).catch(err => {
+                //IPFS化前のデータがあり、エラーが多いので一時的にコメントアウト
+                // console.log('err:', err);
+            });
+    }
+    return outputs
+}
+
+IpfsManager.convertAnswer = function(title,body){
+    var outputs = []
+    for (let i = 0; i < rows.length; i++) {
+        await axios.get('https://cloudflare-ipfs.com/ipfs/' + rows[i].body)
+            .then(response => {
+                outputs.push({body:response.data.body})
+            }).catch(err => {
+                //IPFS化前のデータがあり、エラーが多いので一時的にコメントアウト
+                // console.log('err:', err);
+            });
+    }
+    return outputs
+}
+
+for (let i = 0; i < result.rows.length; i++) {
+    axios.get('https://cloudflare-ipfs.com/ipfs/' + result.rows[i].title)
+        .then(response => {
+            result.rows[i].title = response.data.title;
+            result.rows[i].body = response.data.body;
+        }).catch(err => {
+            //IPFS化前のデータがあり、エラーが多いので一時的にコメントアウト
+            // console.log('err:', err);
+        });
 }
 
 export default IpfsManager;
