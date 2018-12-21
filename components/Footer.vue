81874e7f89b6eea6b7d5cb36abedf26858f98985
Kohei Tanaka <kohei69@gmail.com>
2018-12-21 16:35:16 +0900

    add_terms

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
   <v-footer dark color="teal lighten-2">
     <v-spacer></v-spacer>
+      <nuxt-link to="/terms">terms</nuxt-link>
+      &nbsp;  &nbsp;
       <v-flex>E-Channel &copy; {{ new Date().getFullYear() }}</v-flex>
   </v-footer>
 </template>
